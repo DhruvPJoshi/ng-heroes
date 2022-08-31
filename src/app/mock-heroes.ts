0246d7013053f17efb721ec37ba649cb81de9b0c
Dhruv Joshi <joshidhruv997@gmail.com>
2022-08-31 16:24:25 +0530

    show list of mock heroes and bind click event

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Superman' },
+    { id: 13, name: 'Batman' },
+    { id: 14, name: 'Spider-Man' },
+    { id: 15, name: 'Iron Man' },
+    { id: 16, name: 'Aquaman' },
+    { id: 17, name: 'Captain America' },
+    { id: 18, name: 'Hawkeye' },
+    { id: 19, name: 'The Flash' }
+];
